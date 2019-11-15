ae09287f04e58ce3fbae06e01e655ba93909c9f1
Jordan Shurmer <jordan@shurmer.family>
2019-11-14 22:00:54 -0500

    Refactor Conditional requests to get precedence and HEAD/GET checks correct

---
+++ server/src/http/conditional.rs
@@ -0,0 +1,62 @@
+use std::time::SystemTime;
+
+pub struct Conditional {
+    if_match_header: Option<String>,
+    if_unmodified_header: Option<SystemTime>,
+    
+    if_none_match_header: Option<String>,
+    if_modified_header: Option<SystemTime>,
+
+    last_modified: SystemTime,
+    etag: String,
+}
+
+fn string_header(request: &hyper::Request<hyper::Body>, header: &str) -> Option<String> {
+    request.headers()
+        .get(header)
+        .and_then(|value| value.to_str().ok())
+        .map(|s| s.to_owned())
+}
+
+fn date_header(request: &hyper::Request<hyper::Body>, header: &str) -> Option<SystemTime> {
+    request.headers()
+        .get(header)
+        .and_then(|value| value.to_str().ok())
+        .and_then(|s| httpdate::parse_http_date(s).ok())
+}
+
+impl Conditional {
+
+    pub fn new(request: &hyper::Request<hyper::Body>, last_modified: SystemTime, etag: String) ->  Self {
+
+        Conditional {
+            if_match_header: string_header(request, "If-Match"),
+            if_unmodified_header: date_header(request, "If-Unmodified-Since"),
+            if_none_match_header: string_header(request, "If-None-Match"),
+            if_modified_header: date_header(request, "If-Modified-Since"),
+
+            last_modified,
+            etag,
+        }
+    }
+
+    pub fn if_match(&self) -> Option<bool> {
+        self.if_match_header.as_ref()
+            .map(|header| &self.etag == header)
+    }
+
+    pub fn if_unmodified_since(&self) -> Option<bool> {
+        self.if_unmodified_header.as_ref()
+            .map(|header| &self.last_modified <= header)
+    }
+
+    pub fn if_none_match(&self) -> Option<bool> {
+        self.if_none_match_header.as_ref()
+        .map(|header| !header.split(",").any(|value| &self.etag == value.trim()))
+    }
+
+    pub fn if_modified_since(&self) -> Option<bool> {
+        self.if_modified_header.as_ref()
+        .map(|header| &self.last_modified > header)
+    }
+}
--- server/src/http/mod.rs
+++ server/src/http/mod.rs
@@ -1,7 +1,10 @@
+mod conditional;
+
 use crate::error::Error;
 use crate::error::Kind::*;
+use conditional::Conditional;
 use core::hash::{Hash, Hasher};
-use hyper::{Body, Request, Response, StatusCode};
+use hyper::{Body, Method, Request, Response, StatusCode};
 use std::collections::hash_map::DefaultHasher;
 use std::path::PathBuf;
 use tokio::fs::metadata;
@@ -15,7 +18,6 @@ pub struct Resource {
 }
 
 impl Resource {
-
     // create a resource from an http request
     pub async fn from_request(request: &Request<Body>) -> Result<Self, Error> {
         let file_path = PathBuf::from(request.uri().path().trim_start_matches('/'));
@@ -27,49 +29,58 @@ impl Resource {
         last_modified.hash(&mut h);
         let etag = h.finish().to_string();
 
+        let conditional = Conditional::new(request, modified_time, etag.clone());
+        let get_or_head = request.method() == &Method::GET || request.method() == &Method::HEAD;
+
         // Conditional HTTP Request checks
         //  (return Err if it fails indicating why in the `kind` of error)
-        if let Some(request_header) = request.headers().get("If-Match") {
-            if etag != request_header.to_str().unwrap_or_default() {
-                return Err(Error {
-                    kind: PreconditionFailed,
-                });
-            }
-        }
-
-        if let Some(request_header) = request.headers().get("If-Unmodified-Since") {
-            if let Ok(header_value) = httpdate::parse_http_date(request_header.to_str().unwrap_or_default()) {
-                if modified_time > header_value {
+        match conditional.if_match() {
+            Some(does_match) => {
+                if !does_match {
                     return Err(Error {
                         kind: PreconditionFailed,
-                    })
-                }
-            }
-        }
-        
-        if let Some(request_header) = request.headers().get("If-None-Match") {
-            for header_tag in request_header.to_str().unwrap_or_default().split(",") {
-                if etag == header_tag.trim() {
-                    return Err(Error {
-                        kind: NotModified
                     });
                 }
             }
-        }
 
-        if let Some(request_header) = request.headers().get("If-Modified-Since") {
-            if let Ok(header_value) = httpdate::parse_http_date(request_header.to_str().unwrap_or_default()) {
-                if modified_time <= header_value {
-                    return Err(Error {
-                        kind: NotModified,
-                    });
+            // Only check If-Unmodified-Since when there was no If-Match given (per the spec)
+            None => {
+                if let Some(fresh) = conditional.if_unmodified_since() {
+                    if !fresh {
+                        return Err(Error {
+                            kind: PreconditionFailed,
+                        });
+                    }
                 }
             }
         }
 
-        // TODO: fix the precedence... e.g. If-Modified-Since should be ignored if there is an If-None-Match
-        // TODO: Range requests
+        match conditional.if_none_match() {
+            Some(none_match) => {
+                if !none_match {
+                    if get_or_head {
+                        return Err(Error { kind: NotModified });
+                    } else {
+                        return Err(Error {
+                            kind: PreconditionFailed,
+                        });
+                    }
+                }
+            }
 
+            // Only check If-Modified-Since when there was no If-None-Match given (per the spec)
+            None => {
+                if get_or_head {
+                    if let Some(newer) = conditional.if_modified_since() {
+                        if !newer {
+                            return Err(Error {
+                                kind: NotModified,
+                            });
+                        }
+                    }
+                }
+            }
+        }
 
         Ok(Self {
             file_path,
